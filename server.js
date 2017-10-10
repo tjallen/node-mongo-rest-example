05f4d40e34ce7282ecd63be3be60ca326931d665
Thom Allen <thomwork@gmail.com>
2017-10-10 14:51:13 +0100

    Update server.js; connect to db, load model, use bodyParser; reg routes

--- server.js
+++ server.js
@@ -1,6 +1,18 @@
 var express = require('express');
 var app = express();
 var port = 3333;
+var mongoose = require('mongoose');
+var Task = require('./api/models/todoListModel');
+var bodyParser = require('body-parser');
+
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/Tododb');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+var routes = require('./api/routes/todoListRoutes');
+routes(app);
 
 app.listen(port);
 
